1cf3a2aa4280ceafef5f263e9d50e16850c089e5
Steve Beaulac <steve@beaulac.me>
2026-07-15 22:42:11 -0400

    refactor(config): modernize fish config, clean up tools/pi settings
    - Switch fish init checks to  from
    - Reorganize editor aliases and remove dead vim aliases under nvim
    - Drop aqua:fish-shell from mise tools
    - Add subagent model overrides to pi settings
    - Bump pi changelog version to 0.80.7

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -17,46 +17,68 @@ if status is-interactive
 
     set -gx npm_config_prefix "$HOME/.local"
 
+    if command -q mise
+        mise activate fish | source
+    end
+
+    if command -q direnv
+        direnv hook fish | source
+    end
+
+    if command -q zoxide;
+        zoxide init fish | source
+    end
+
+    if command -q fzf
+       fzf --fish | source
+       alias zk="nvim \$(fzf --walker=file --walker-root=/home/sjb/Documents/nca-notes/ --preview 'bat --color=always -n --line-range :500 {}')"
+       #set -gx FZF_CTRL_T_OPTS  "--preview 'bat --color=always -n --line-range :500 {}'"
+       #set -gx FZF_ALT_C_OPTS "--preview 'eza --icons=always --color=always {} | head -200'"
+    end
+
+    if command -q jj
+        jj util completion fish | source
+    end
+
     # fish_config theme choose "kanagawa"
     # fish_config theme choose "Dracula"
 
-    command -qv nmcli && alias nmlist "nmcli device wifi list"
-    command -qv tree && alias tree "tree -L 3 -a -I '.git' --charset X "
-    command -qv tree && alias dtree "tree -L 3 -a -d -I '.git' --charset X "
-
-    command -qv wl-copy && alias cwd "wl-copy \$(pwd)"
-    command -qv wl-copy && alias cc  "wl-copy \$(echo \$history[1])"
-
-    command -qv fabric && alias ai "fabric -p \$(fabric-ai -l | fzf)"
-    command -qv xdg-open && alias o "xdg-open"
-
-    command -qv lazygit && alias lg lazygit
-
-    if command -qv nvim;
-        alias vim v
-        alias vi v
-        alias mini "NVIM_APPNAME=mini.nvim nvim"
-        set -gx EDITOR nvim
-    end 
-    if command -qv vim;
-        alias vi vim 
-        alias v vim
-        set -gx EDITOR vim
+    command -q nmcli && alias nmlist "nmcli device wifi list"
+    if command -q tree 
+        alias tree "tree -L 3 -a -I '.git' --charset X "
+        alias dtree "tree -L 3 -a -d -I '.git' --charset X "
     end
 
-    command -qv jj && alias rev "jj log -G -T 'commit_id.short(8)'"
+    if command -q wl-copy 
+        alias cwd "wl-copy \$(pwd)"
+        alias cc  "wl-copy \$(echo \$history[1])"
+    end
 
-    if command -qv shred
+    command -q fabric && alias ai "fabric -p \$(fabric-ai -l | fzf)"
+    command -q xdg-open && alias o "xdg-open"
+
+    command -q lazygit && alias lg lazygit
+
+
+    if command -q nvim
+        set -gx EDITOR nvim
+    else if command -qv vim
+        set -gx EDITOR vim
+    end 
+
+    command -q jj && alias rev "jj log -G -T 'commit_id.short(8)'"
+
+    if command -q shred
         alias nuke "shred -u -n 1" 
     end
     
-    if command -qv wt;
+    if command -q wt
         alias wtc "wt switch --create --execute=$EDITOR"
         alias wts "wt switch"
         alias wtr "wt remove @"
     end
 
-    if command -qv /usr/bin/opencode;
+    if command -q opencode
         alias oc "opencode"
     end
 
@@ -70,29 +92,6 @@ if status is-interactive
     alias g=git
 
     set -gx VISUAL $EDITOR
-
-    if test -x /usr/bin/mise;
-       /usr/bin/mise activate fish | source
-    end
-
-    if test -x /usr/bin/direnv;
-       /usr/bin/direnv hook fish | source
-    end
-
-    if test -x /usr/bin/zoxide;
-       /usr/bin/zoxide init fish | source
-    end
-
-    if test -x /usr/bin/fzf;
-       /usr/bin/fzf --fish | source
-       alias zk="nvim \$(fzf --walker=file --walker-root=/home/sjb/Documents/nca-notes/ --preview 'bat --color=always -n --line-range :500 {}')"
-       #set -gx FZF_CTRL_T_OPTS  "--preview 'bat --color=always -n --line-range :500 {}'"
-       #set -gx FZF_ALT_C_OPTS "--preview 'eza --icons=always --color=always {} | head -200'"
-    end
-
-    if test -x /usr/bin/jj;
-        /usr/bin/jj util completion fish | source
-    end
 
     # NVM
     function __check_nvm --on-variable PWD --description 'Do nvm stuff'
