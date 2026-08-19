f879df74d44aee4ab8b6ec212331e0861615d303
Steve Beaulac <steve@beaulac.me>
2026-08-19 14:33:26 -0400

    refactor(nvim): switch to pi2.nvim and add render-markdown

--- config/nvim/plugin/pi.lua
+++ config/nvim/plugin/pi.lua
@@ -1,9 +1,8 @@
-vim.pack.add({ "https://github.com/alex35mil/pi.nvim" })
-
-vim.keymap.set({'n'}, '<leader>mp', '<cmd>Pi<cr>',  { noremap = true, silent = true, desc = "Toggle Pi Agent" })
-require("pi").setup({
-    cli = {
-        args = { "--approve" },
-    },
+vim.keymap.set({ 'n' }, '<leader>mp', '<cmd>Pi<cr>', { noremap = true, silent = true, desc = "Toggle Pi Agent" })
+vim.pack.add({
+    "https://github.com/zgs225/pi2.nvim",
+    -- Default chat-history renderer (render.engine = "render-markdown"):
+    "https://github.com/MeanderingProgrammer/render-markdown.nvim",
 })
 
+require("pi").setup()
