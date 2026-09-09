5fbbcd81fdb47bfaac74b0e9d3969bce85111227
Steve Beaulac <steve@beaulac.me>
2026-09-09 18:34:46 +0000

    feat(omarchy): add default Tokyo Night theme
    Add omarchy theme files under config/omarchy/default: terminal
    (alacritty, kitty, foot, ghostty), editor (helix, neovim), bar/launcher
    (waybar, walker, rofi, mako), and supporting colors, backgrounds, and
    preview images. theme.name set to 'default'.

---
+++ config/omarchy/default/hyprland.lua
@@ -0,0 +1,14 @@
+hl.config({
+    general = {
+        col = {
+            active_border = { colors = {"rgb(7aa2f7)", "rgb(f7768e)" }, angle = 45 },
+            inactive_border = { colors = {"rgb(9ece6a)", "rgb(9ece6a)" }, angle = 45 }
+        }
+    },
+    group = {
+        col = {
+            border_active = { colors = {"rgb(7aa2f7)", "rgb(f7768e)" }, angle = 45 },
+            border_inactive = { colors = {"rgb(9ece6a)", "rgb(9ece6a)" }, angle = 45 }
+        }
+    }
+})
---
+++ config/omarchy/default/neovim-vimpack.lua
@@ -0,0 +1,12 @@
+vim.pack.add({{ src = 'https://github.com/folke/tokyonight.nvim' }})
+require("tokyonight").setup({
+    style = "night",
+    transparent = true,
+    terminal_colors = true,
+    styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+    },
+})
+
+vim.cmd("colorscheme tokyonight")
